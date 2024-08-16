ffd13c359e757bfe633598d5b5b29543595e1089
allemas <sebastien.a.consulting@gmail.com>
2024-08-16 18:23:52 +0200

    Bootstrap flamegraph, file located at /tmp/flamegraph.html

--- core/src/main/java/com/byteprofile/core/StackTraceProfiler.java
+++ core/src/main/java/com/byteprofile/core/StackTraceProfiler.java
@@ -1,8 +1,10 @@
 package com.byteprofile.core;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class StackTraceProfiler implements Runnable {
+    Store store = new Store();
 
 
     /**
@@ -13,15 +15,28 @@ public class StackTraceProfiler implements Runnable {
     public void run() {
         while (true) {
             Thread.getAllStackTraces().forEach((thread, stackTraceElements) -> {
-                System.out.println("=====" + thread.getName() + "==");
-                Arrays.stream(stackTraceElements).forEach(stackTraceElement ->
-                {
-                    System.out.println(stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName());
-                });
+                store.addSample(stackTraceElements);
+                /**
+                 System.out.println("=====" + thread.getName() + "==");
+                 Arrays.stream(stackTraceElements).forEach(stackTraceElement ->
+                 {
+                 System.out.println(stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName());
+                 });
+                 **/
             });
 
             try {
                 Thread.sleep(2000);
+                store.methodSampleCount.forEach((name, count) -> {
+                    System.out.println(String.format("%s : %d", name, count));
+                });
+
+                try {
+                    store.createFlameGraphFile();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             } catch (InterruptedException e) {
                 break;
             }
